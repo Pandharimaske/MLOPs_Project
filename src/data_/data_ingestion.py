0ec7ddebc8368fee1707aa291e3a28934868b45e
Pandhari <pandhari2527@gmail.com>
2025-09-26 00:40:22 +0530

    ci 4

--- src/data_/data_ingestion.py
+++ src/data_/data_ingestion.py
@@ -79,9 +79,9 @@ def main():
         test_size = params['data_ingestion']['test_size']
         # test_size = 0.2
 
-        # df = load_data(data_url='https://raw.githubusercontent.com/vikashishere/Datasets/refs/heads/main/data.csv')
-        s3 = s3_connection.s3_operations("mlops-project-bucket-pandhari" , os.getenv("AWS_ACCESS_KEY") , os.getenv("AWS_SECRET_KEY"))
-        df = s3.fetch_file_from_s3("IMDB Dataset.csv")
+        df = load_data(data_url='https://raw.githubusercontent.com/vikashishere/Datasets/refs/heads/main/data.csv')
+        # s3 = s3_connection.s3_operations("mlops-project-bucket-pandhari" , os.getenv("AWS_ACCESS_KEY") , os.getenv("AWS_SECRET_KEY"))
+        # df = s3.fetch_file_from_s3("IMDB Dataset.csv")
 
         final_df = preprocess_data(df)
         train_data , test_data = train_test_split(final_df , test_size=test_size , random_state=42)
